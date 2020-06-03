e6188d423854a32a15a54414e2faac7ce84c317b
ratana176 <chhormratana@gmail.com>
2020-06-03 08:00:46 +0700

    update url

--- app/route/web.php
+++ app/route/web.php
@@ -6,3 +6,12 @@ Route::get('/', 'HomeController@index');
 Route::get('/1', 'HomeController@index');
 Route::get('/4', 'HomeController@index');
 Route::get('/3', 'HomeController@index');
+
+
+/*
+
+    $content ="<p>This is a sample text where {123456} and {7894560} ['These are samples']{145789}</p>";
+    preg_match_all('/{(.*?)}/', $content, $matches);
+    print_r(array_map('intval',$matches[1]));
+
+*/
